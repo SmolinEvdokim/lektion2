744170d7304fb2f9a02a97cfa20a44f40ca5c440
SmolinEvdokim <smolinevdok@gmail.com>
2022-12-09 23:11:07 +0100

    nahozdenie_max_iz9_4isel

--- nahozdenie_max_iz9_4isel/Program.cs
+++ nahozdenie_max_iz9_4isel/Program.cs
@@ -9,13 +9,13 @@ int b3 = 23;
 int c3 = 33;
 
 int max = a1;
-if(b1 > max ) max = b1;
-if(c1 > max ) max = c1;
-if(a2 > max ) max = a2;
-if(b2 > max ) max = b2;
-if(c2 > max ) max = c2;
-if(a3 > max ) max = a3;
-if(b3 > max ) max = b3;
-if(c3 > max ) max = c3;
+if(b1 > max) max = b1;
+if(c1 > max) max = c1;
+if(a2 > max) max = a2;
+if(b2 > max) max = b2;
+if(c2 > max) max = c2;
+if(a3 > max) max = a3;
+if(b3 > max) max = b3;
+if(c3 > max) max = c3;
  
-Console.Writeline(max);
+Console.WriteLine(max);

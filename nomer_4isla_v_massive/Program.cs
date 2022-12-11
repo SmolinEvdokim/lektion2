39915f081a1d85fe0f677cb52119b87c3537229d
SmolinEvdokim <smolinevdok@gmail.com>
2022-12-11 11:56:39 +0100

    nommer v massive/21.1

--- nomer_4isla_v_massive/Program.cs
+++ nomer_4isla_v_massive/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = { 1, 18, 31, 4, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;  // naiti eto 4islo v massive
+
+int index = 0;// ustanovka i na4alo s4et4ika 
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // esli nujno tolko odin raz
+    }
+    index = index + 1; // ili index = index++;
+
+}
